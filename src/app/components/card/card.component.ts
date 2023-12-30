25d85bdccbd77ab4ee64cfdbeb33f837e99fa1d9
Paulo Zanluqui <paulozanluqui13@gmail.com>
2023-12-30 18:30:44 -0300

    first commit

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  @Input()
+  gameCover: string ="";
+  @Input()
+  gameLabel: string = "";
+  @Input()
+  gameType: string = "Digital PS4";
+  @Input()
+  gamePrice: string = "399,90";
+  
+}
